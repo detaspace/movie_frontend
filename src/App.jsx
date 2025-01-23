8b9a7c202f343fbe9410a274d836f3819e8db314
Mohit Singh Raghav <118093505+asima2006@users.noreply.github.com>
2025-01-23 19:47:49 +0530

    refactor: update App component structure and route path

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import VideoPlayer from './components/VideoPlayer';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import VideoPlayer from "./components/VideoPlayer";
 
-const App = () => (
-  <Router>
-    <Routes>
-      <Route path="/:id" element={<VideoPlayer />} />
-    </Routes>
-  </Router>
-);
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/video/:id" element={<VideoPlayer />} />
+        {/* Add other routes as needed */}
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
