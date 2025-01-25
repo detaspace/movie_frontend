d84432687861e507bf36058ea0167e31b14982fb
Mohit Singh <118093505+asima2006@users.noreply.github.com>
2025-01-25 21:15:28 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import VideoPlayer from "./components/videoPlayer";
+import VideoPlayer from "./components/VideoPlayer";
 import Navbar from "./components/Navbar";
 
 const App = () => {
