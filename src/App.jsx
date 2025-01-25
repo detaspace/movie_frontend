27345b38ecd246697c54422dd69ef26f2aa8b13c
Mohit Singh Raghav <118093505+asima2006@users.noreply.github.com>
2025-01-25 21:14:19 +0530

    feat: update import path for VideoPlayer and add Navbar component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import VideoPlayer from "./components/VideoPlayer";
+import VideoPlayer from "./components/videoPlayer";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <Router>
+      <Navbar/>
       <Routes>
         <Route path="/video/:id" element={<VideoPlayer />} />
         {/* Add other routes as needed */}
